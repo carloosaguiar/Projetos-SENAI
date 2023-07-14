163da4ddd5002c85a6504cccc348dea6b249da79
carloosaguiar <ceduardoroagui@gmail.com>
2023-07-14 08:30:26 -0300

    Ok

---
+++ Projeto LHPet/LHPets/Models/Contexto.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace LHPets.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options){
+
+        }
+
+        public DbSet<Cliente> Cliente {get; set;}
+    }
+}
---
+++ Projeto_Web_Lh_Pets_versao_1/Program.cs
@@ -0,0 +1,29 @@
+namespace Projeto_Web_Lh_Pets_versao_1{
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Projeto Web – LH Pets versão 1");
+
+            app.UseStaticFiles();
+
+            app.MapGet("/index", (HttpContext context) => {
+                context.Response.Redirect("index.html", false);
+            });
+
+            Banco dba = new Banco();
+
+            dba.BuscarBanco();
+
+            app.MapGet("/listaClientes", (HttpContext context) =>{
+                context.Response.WriteAsync(dba.GetListaString());
+            });
+
+            app.Run();
+        }
+    }
+}
